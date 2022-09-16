e96242a62881a7779f81fb9d36238e7b98770753
D.석수호 <suho.seok@supercat.co.kr>
2022-09-16 17:59:04 +0900

    edit 루아 모듈 로더

--- example/module_loader.lua
+++ example/module_loader.lua
@@ -1,7 +1,16 @@
 ---@module module_loader 모듈 로더
-return {
+local modules = {
     case1 = require ("lib/test_module1"),
     case2 = require ("lib/test_module2"),
     case3 = require ("lib/test_module3"),
     case4 = require ("lib/test_module4")
 }
+
+-- logger
+for _, module in pairs(modules) do
+    if module ~= nil then
+        print("success".. case1 .. "is loaded")
+    end
+end
+
+return modules
