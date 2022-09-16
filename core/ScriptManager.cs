cbbe36689f8f2aa93340db911984fb3e3fd72850
Spiegel <turbogara1@gmail.com>
2022-09-17 01:42:42 +0900

    폴더 정리

--- core/ScriptManager.cs
+++ core/ScriptManager.cs
@@ -9,21 +9,6 @@ using MoonSharp.Interpreter;
 
 namespace MoonSharpDemo
 {
-    /// <summary xml:lang="ko">
-    /// 각 루아 파일이 담겨져 있는 클래스입니다.
-    /// </summary>
-    public class LuaFile
-    {
-        public string Context { get; }
-        public bool IsModule { get; }
-        public DynValue Cache { get; set; }
-        public LuaFile(string context, bool isModule = false)
-        {
-            Context = context;
-            IsModule = isModule;
-        }
-    }
-
     public static class ScriptManager
     {
         private static readonly string RootDir = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName, "example");
