f02169bc026e3a9adf04528ce150d44bee45de63
Spiegel <turbogara1@gmail.com>
2022-09-17 06:31:40 +0900

    rename test files

--- example/module_loader.lua
+++ example/module_loader.lua
@@ -1,14 +1,15 @@
 ---@module module_loader 모듈 로더
 local modules = {
-    case1=require "lib/test_module1",
-    case2 = require 'lib/test_module2',
-    case3 = require ('lib/test_module3'),
-    case4 = require ("lib/test_module4"),
-    case5 = require"lib/test_module5",
-    case6 = require'lib/test_module6',
-    case7 = require'lib/test_module6',
-    case8 = require'lib/test_module6',
-    case9 = require'lib/test_module6',
+    case1=require "lib/module1",
+    case2 = require 'lib/module2',
+    case3 = require ('lib/module3'),
+    case4 = require ("lib/module4"),
+    case5 = require"lib/module5",
+    case6 = require'lib/module6',
+    case7 = require'lib/module6',
+    case8 = require'lib/module6',
+    case9 = require'lib/module6',
+    case10 = require'lib/function1',
 }
 
 -- logger
